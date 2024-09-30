30ff47b09da2380d46e9bfe1ab2e8a002897a28b
Paweł Mikłas <pablomiklas@gmail.com>
2024-09-30 21:53:45 +0200

    fix: avoid fetching when error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UsersList } from "./pages/Users";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 const App = () => {
   return (
