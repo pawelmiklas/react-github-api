0acc7c60c3ecb9cc439c6bc6f9754c76f439c25d
Paweł Mikłas <pablomiklas@gmail.com>
2024-10-01 20:26:32 +0200

    feat: display more information in table

--- src/pages/Users/components/UsersTable.tsx
+++ src/pages/Users/components/UsersTable.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Typography } from "@mui/material";
+import { Avatar, Typography, Link } from "@mui/material";
 import { User } from "../../../types";
 import { BaseTable, Column } from "../../../components/BaseTable";
 
@@ -20,6 +20,32 @@ export const UsersTable = ({ isLoading, data }: UsersTableProps) => {
       key: "login",
       header: "Username",
       render: ({ login }) => <Typography>{login}</Typography>,
+    },
+    {
+      key: "html_url",
+      header: "Profile",
+      render: ({ html_url, login }) => (
+        <Link href={html_url} target="_blank" rel="noopener noreferrer">
+          {login}'s GitHub
+        </Link>
+      ),
+    },
+    {
+      key: "type",
+      header: "Type",
+      render: ({ type }) => <Typography>{type}</Typography>,
+    },
+    {
+      key: "site_admin",
+      header: "Admin",
+      render: ({ site_admin }) => (
+        <Typography>{site_admin ? "Yes" : "No"}</Typography>
+      ),
+    },
+    {
+      key: "score",
+      header: "Score",
+      render: ({ score }) => <Typography>{score.toFixed(2)}</Typography>,
     },
   ];
 
